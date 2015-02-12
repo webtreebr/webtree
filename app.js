5e5bb5add82ad61226d9dcbb32ffb1868008b6b6
Fábio Akira Yoshida <fabioseno@gmail.com>
2015-02-11 23:05:04 -0200

    Flexbox

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-/*global require, process*/
+/*global require, process, console*/
 var express = require('express'),
+    favicon = require('serve-favicon'),
     app = express(),
     port = process.env.PORT || 8080;
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/favicon.ico'));
 
 app.get('/', function (req, res) {
     'use strict';
@@ -14,3 +17,4 @@ app.get('/', function (req, res) {
 });
 
 app.listen(port);
+console.log('Listening on port ' + port);
